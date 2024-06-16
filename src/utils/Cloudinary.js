7f8f8502a38264a726ceec47c647331f936471f0
Shivansh Barthwal <shivanshbarthwal3.14@gmail.com>
2024-06-16 12:41:00 +0530

    cloudinary and multer

---
+++ src/utils/Cloudinary.js
@@ -0,0 +1,33 @@
+import {v2 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+cloudinary.config({
+    cloud_name : process.env.cloud_name,
+    api_key: process.env.api_key,
+    api_secret : process.env.api_secret,
+});
+
+
+const uploadonCloudinary = async(localfilepath) => {
+    try {
+        if (!localfilepath)   return null
+        // upload on cloudinary 
+        const response = await cloudinary.uploader.upload(localfilepath , {
+            resource_type : "auto"
+        })
+
+        //file has been uploaded successfully
+        console.log("file has been uploaded succesfully" , response.url);
+
+        return response
+
+        
+    } catch (error) {
+        fs.unlinkSync(localfilepath) // remove the locally saved temporary file as the upload operation got failed
+        return null
+
+
+    }
+}
+
